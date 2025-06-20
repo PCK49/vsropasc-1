c39c6a482b7018f39d4bc45858cadbd1755ed470
PCK49 <160991878+PCK49@users.noreply.github.com>
2025-06-20 14:58:19 -0700

    Update rps.js

--- rps.js
+++ rps.js
@@ -1,8 +1,11 @@
 const choices=["rock","paper","scissors"]
-const computerChoices=choices[math.floor(Math.random()*choices*length)];
+const computerChoices=choices[Math.floor(Math.random()*choices*.length)];
 function rock(){
+  const playersChoice="rock";
 }
 function paper(){
+  const playersChoice="paper";
 }
 function scissors(){
+  const playersChoice="scissors";
 }
